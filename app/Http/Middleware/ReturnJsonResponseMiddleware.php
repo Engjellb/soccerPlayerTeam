7cf96a9b1c5c7993d8b85d83fe8764c11fa85b06
Engjellb <engjibeselica@gmail.com>
2022-11-22 15:27:12 +0100

    [feature/api-and-files-structure] Addition of a middleware to return api responses in json format

---
+++ app/Http/Middleware/ReturnJsonResponseMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ReturnJsonResponseMiddleware
+{
+    /**
+     * Handle an incoming api request.
+     * Make response to be returned in json.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $acceptHeader = $request->header('Accept');
+
+        if ($acceptHeader !== 'application/json') {
+            $request->headers->set('Accept', 'application/json');
+        }
+
+        return $next($request);
+    }
+}
