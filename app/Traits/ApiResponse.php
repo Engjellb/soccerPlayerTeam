e459a5b2a80b60a4e29e69dda5bb232bba2ceb70
Engjellb <engjibeselica@gmail.com>
2022-12-01 14:54:03 +0100

    [feature/api-error-handling] Addition of a successful and error response in trait

---
+++ app/Traits/ApiResponse.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+trait ApiResponse
+{
+    /**
+     * Return a successful json response in controllers
+     *
+     * @param JsonResource $data
+     * @param string|null $message
+     * @param int $code
+     * @return JsonResponse
+     */
+    public function successResponse(JsonResource $data, string $message = null, int $code = 200): JsonResponse
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+
+    /**
+     * Return an error json response in controllers
+     *
+     * @param int $code
+     * @param string|null $message
+     * @return JsonResponse
+     */
+    public function errorResponse(int $code, string $message = null): JsonResponse
+    {
+        return response()->json([
+           'status' => 'error',
+           'message' => $message,
+           'data' => null
+        ], $code);
+    }
+}
